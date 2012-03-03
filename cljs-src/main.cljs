3753be3aa8b8962afff2f9c7822be95328329deb
Chris McBride <cmcbride@rjmetrics.com>
2012-03-03 02:47:31 -0500

    added waltz, advanced opt broke

--- cljs-src/main.cljs
+++ cljs-src/main.cljs
@@ -1,56 +1,66 @@
 (ns clj-websocket-demo.main
-  (:require [clj-websocket-demo.websocket :as sock])
-  (:use [jayq.core :only [$ bind append]]
+  (:require [clj-websocket-demo.websocket :as sock]
+            [waltz.state :as state])
+  (:use [jayq.core :only [$ bind append show hide]]
         [cljs.reader :only [read-string]]
-        [clj-websocket-demo.js-utils :only [current-timestamp-ms]]
-        [crate.core :only [html]]))
+        [clj-websocket-demo.js-utils :only [log current-timestamp-ms]]
+        [crate.core :only [html]])
+  (:use-macros [waltz.macros :only [in out defstate deftrans]]))
 
 (def $body ($ :body))
 (def $socket-output ($ :#socketOutput))
-(def $record-button ($ :#record))
-(def $stop-button ($ :#stop))
-(def $play-button ($ :#play))
-
 (def user-id (rand-int 9999999999))
 (def record-url (str user-id "/record"))
 (def play-url (str user-id "/play"))
-
-(def is-recording? (atom false))
 (def inital-timestamp-ms (atom 0))
+(def sm (state/machine "das"))
+(state/set-debug sm false)
+
+(defn update-socket-status [msg]
+  (append $socket-output (str msg "\n")))
 
 (defn draw-mouse [eventMap]
   (append $body 
     (html [:div {:class "mouseLine" :style (str "left:" (:x eventMap) "px;"
                                                 "top:" (:y eventMap) "px;")}])))
 
-(bind-mousemove (sock/create-socket record-url))
+(defstate sm :stop
+    (in [] (do
+      (hide ($ :#stop))
+      (update-socket-status "stoppped recording")))
+    (out [] (show ($ :#stop))))
 
-(bind $record-button :click 
-  (fn [] 
-    (update-socket-status "recording")
-    (reset! is-recording? true)
-    (reset! inital-timestamp-ms (current-timestamp-ms))))
+(defstate sm :record
+    (in [] (do
+      (hide ($ :#record))
+      (update-socket-status "recording")
+      (reset! inital-timestamp-ms (current-timestamp-ms))))
+    (out [] (show ($ :#record))))
 
-(bind $stop-button :click 
-  (fn []
-    (update-socket-status "stoppped recording") 
-    (reset! is-recording? false)))
+(defstate sm :play
+    (in [] (do
+      (hide ($ :#play))
+      (update-socket-status "playing")
+      (sock/create-socket play-url {:on-message #(draw-mouse (read-string (.-data %)))})))
+    (out [] (show ($ :#play))))
 
-(bind $play-button :click
-  (fn []
-    (update-socket-status "playing")
-    (reset! is-recording? false)
-    (sock/create-socket play-url {:on-message #(draw-mouse (read-string (.-data %)))
-                                  :on-close #(update-socket-status "Playback finished")})))
+(deftrans sm :set [new-state]
+  (state/unset sm (state/current sm))
+  (state/set sm new-state))
 
-(defn bind-mousemove [socket]
+(state/transition sm :set :stop)
+
+(bind ($ :button) :click
+  (fn [e]
+    (let [new-state (-> (. e -target) (.-id))]
+      (state/transition sm :set (keyword new-state)))))
+
+(let [socket (sock/create-socket record-url)]
   (bind $body :mousemove
     (fn [e]
-      (when @is-recording?
+      (when (state/in? sm :record)
         (sock/send! socket 
           {:x (. e -pageX)
            :y (. e -pageY)
            :offset (- (current-timestamp-ms) @inital-timestamp-ms)})))))
 
-(defn update-socket-status [msg]
-  (append $socket-output (str msg "\n")))
--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :dependencies [
     [org.clojure/clojure "1.4.0-beta3"]
     [crate "0.1.0-SNAPSHOT"]
+    [waltz "0.1.0-SNAPSHOT"]
     [noir-async "0.1.4"]
     [jayq "0.1.0-SNAPSHOT"]]
   :plugins [[lein-cljsbuild "0.1.1"]]
@@ -12,5 +13,6 @@
       :jar true
       :compiler {
         :output-to "resources/public/js/cljs.js"
-        :optimizations :advanced
+ ;       :optimizations :advanced
+        :optimizations :simple
         :pretty-print true}}]})
